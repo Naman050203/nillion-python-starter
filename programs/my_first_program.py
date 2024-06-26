dff4a846a713fda0ddefabdaccca98c5fcdaf239
Naman050203 <naman.mishra2021@vitbhopal.ac.in>
2024-06-26 11:11:57 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,15 +1,19 @@
 from nada_dsl import *
 
 def nada_main():
-    # Define the party
+    
     party1 = Party(name="Party1")
 
-    # Define secret integer inputs for the party
+   
     secret_int1 = SecretInteger(Input(name="my_int1", party=party1))
     secret_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Perform the addition of the two secret integers
+    
     addition_result = secret_int1 + secret_int2
 
-    # Return the result as an output for the party
-    return [Output(addition_result, "addition_output", party1)]
+    
+    return [Output(value=addition_result, name="addition_output", party=party1)]
+
+if __name__ == "__main__":
+    result = nada_main()
+    print("Result:", result)
